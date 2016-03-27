2f7293d33d1fce99c1fb95f5b695b23d0a017c13
andantonyan <andantonyan@gmail.com>
2016-03-27 22:29:49 +0400

    feat(test): add test for gcd function

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,13 @@ pub fn gcd(p: i32, q: i32) -> i32 {
     gcd(q, p % q)
   }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn gcd_test() {
+      assert_eq!(3, gcd(6, 9));
+    }
+}
